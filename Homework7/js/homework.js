b6df2285e273f601b79c108bc09f31e960d8d75e
Sumtsov-s_portfolio <kirillsumcov14@gmail.com>
2024-06-16 09:16:10 +0300

    Homework 7

---
+++ Homework7/js/homework.js
@@ -0,0 +1,19 @@
+// №1
+// let ladder = {
+//     step: 0,
+//     up: function () {
+//         this.step += 1;
+//         return this;
+
+//     },
+//     down: function () {
+//         this.step -= 1;
+//         return this;
+//     },
+//     showStep: function () {
+//         console.log(this.step);
+//         return this;
+//     },
+// };
+
+// ladder.up().up().down().showStep();
